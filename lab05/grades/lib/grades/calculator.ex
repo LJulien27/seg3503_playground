f612fc05b1781a1634f9a37dfb0b409401b72347
LJulien27 <ljuli048@uOttawa.ca>
2023-07-11 20:58:11 -0400

    all changes:

--- lab05/grades/lib/grades/calculator.ex
+++ lab05/grades/lib/grades/calculator.ex
@@ -1,11 +1,11 @@
 defmodule Grades.Calculator do
-    def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-      Enum.random(0..100)
-    end
-
     def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
       arr = ["EIN", "F", "E", "D", "D+", "C", "C+", "B", "B+", "A-", "A", "A+"]
       Enum.take_random(arr, 1)
+    end
+
+    def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+      Enum.random(0..100)
     end
 
     def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
